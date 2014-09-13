65f1a1683cdf48965201f0ae0a5eef7714d6103b
Pulkit Jalan <pulkit1990@gmail.com>
2014-09-13 20:43:05 +0100

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -3,50 +3,50 @@
 return [
 
     /*
-	|--------------------------------------------------------------------------
-	| Enable raven logger
-	|--------------------------------------------------------------------------
-	|
-	| Enable raven logger or not
-	|
-	*/
+    |--------------------------------------------------------------------------
+    | Enable raven logger
+    |--------------------------------------------------------------------------
+    |
+    | Enable raven logger or not
+    |
+    */
     'enabled' => true,
 
     /*
-	|--------------------------------------------------------------------------
-	| Raven DSN
-	|--------------------------------------------------------------------------
-	|
-	| Your project's DSN, found under 'API Keys' in your project's settings.
-	|
-	*/
+    |--------------------------------------------------------------------------
+    | Raven DSN
+    |--------------------------------------------------------------------------
+    |
+    | Your project's DSN, found under 'API Keys' in your project's settings.
+    |
+    */
 
     'dsn' => '',
 
     /*
-	|--------------------------------------------------------------------------
-	| Log Level
-	|--------------------------------------------------------------------------
-	|
-	| Log level at which to log to Sentry. Default `error`.
-	|
-	| Available: 'debug', 'info', 'notice', 'warning', 'error',
-	| 			 'critical', 'alert', 'emergency'
-	|
-	*/
+    |--------------------------------------------------------------------------
+    | Log Level
+    |--------------------------------------------------------------------------
+    |
+    | Log level at which to log to Sentry. Default `error`.
+    |
+    | Available: 'debug', 'info', 'notice', 'warning', 'error',
+    |            'critical', 'alert', 'emergency'
+    |
+    */
 
     'level' => 'error',
 
     /*
-	|--------------------------------------------------------------------------
-	| Queue connection
-	|--------------------------------------------------------------------------
-	|
-	| Choose a custom queue connection to use from your config/queue.php
-	|
-	| Defaults to the default connection and queue from config/queue.php
-	|
-	*/
+    |--------------------------------------------------------------------------
+    | Queue connection
+    |--------------------------------------------------------------------------
+    |
+    | Choose a custom queue connection to use from your config/queue.php
+    |
+    | Defaults to the default connection and queue from config/queue.php
+    |
+    */
 
     'queue' => [
         'connection' => '',
@@ -83,17 +83,17 @@ return [
     ],
 
     /*
-	|--------------------------------------------------------------------------
-	| Raven Configuration
-	|--------------------------------------------------------------------------
-	|
-	| Any values below will be passed directly as configuration to the Raven
-	| instance. For more information about the possible values check
-	| out: https://github.com/getsentry/raven-php
-	|
-	| Example: "name", "tags", "trace", "timeout", "exclude", "extra", ...
-	|
-	*/
+    |--------------------------------------------------------------------------
+    | Raven Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Any values below will be passed directly as configuration to the Raven
+    | instance. For more information about the possible values check
+    | out: https://github.com/getsentry/raven-php
+    |
+    | Example: "name", "tags", "trace", "timeout", "exclude", "extra", ...
+    |
+    */
 
     'options' => []
 
